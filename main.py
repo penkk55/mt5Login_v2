d448c69472c864d2b51cf9876b9185e7ef0e9771
Penk-- <pongparn.c@ns.co.th>
2023-01-07 21:13:53 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# # This is a sample Python script.
+#
+# # Press Shift+F10 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+
+import MetaTrader5 as mt5
+# display data on the MetaTrader 5 package
+print("MetaTrader5 package author: ",mt5.__author__)
+print("MetaTrader5 package version: ",mt5.__version__)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # print_hi('PyCharm')
+
+    print('login_mt5')
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
